3d25091f1cf285d5396f26f68cc38a8d96bcdea6
Toshif <49535483+Toshifkhan@users.noreply.github.com>
2022-11-19 10:11:59 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,41 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./component/TodoInput";
 import TodoList from "./component/TodoList";
 
-//Hello this is the main files
+
 
 export default class App extends Component {
+  state = {
+    items:[{id:1,title:"wake up"},{id:2,title:"make breakfast"}],
+    id:uuid(),
+    item:' ',
+    editItem:false
+  };
+  handleChange = (e) => {console.log('handle changes')}
+  handleSubmit = (e) => {console.log('handle submit')}
+  clearList = () => {console.log('clear list')}
+  handleDelete = (id) => {console.log(`handle edit ${id}`)}
+  handleEdit = (id) => {console.log(`handle edit ${id}`)}
   render() {
     return (
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
-        </div>
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center"> todo input</h3>
+          
+          <TodoInput 
+          item={this.state.item}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          editItem={this.state.editItem}
+          />
+          <TodoList 
+          items={this.state.items }
+          clearList={this.clearList}
+          handleDelete={this.handleDelete}
+          handleEdit={this.handleEdit}
+          />
+          </div>
+       </div>
       </div>
     );
   }
--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
-    return <div>hello from the todo Input</div>;
+    const {item,handleChange,handleSubmit,editItem} = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
